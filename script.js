0f669e46cffc708b45b0f3b401522a02a99dc7d7
Manmeet Singh <90761985+cyraxcodes@users.noreply.github.com>
2025-08-30 00:06:51 +0530

    updated script.js
    added a function that displays a div upon hovering on a row element

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var fixed_con = document.querySelector("#fixed-element-image")
+var element_con = document.querySelector("#row-content")
+
+element_con.addEventListener("mouseenter", function(){
+    fixed_con.style.display = "block"
+})
+
+element_con.addEventListener("mouseleave", function(){
+    fixed_con.style.display = "none"
+})
+
+var elements = document.querySelectorAll(".element")
+console.log(elements)
+elements.forEach(function(x){
+    x.addEventListener("mouseenter", function(){
+        var imgPath = x.getAttribute("data-img")
+        fixed_con.style.backgroundImage = `url(${imgPath})`
+    })
+})
+
+
 // applying smooth scroll on our main wrapper using locomotive js library
 
 const scroll = new LocomotiveScroll({
